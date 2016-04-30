75e7c33488158f27b7eb1c8be6e479ec9f556bd7
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2016-05-01 00:45:08 +0200

    flake8

--- brus/liste/forms.py
+++ brus/liste/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
+
 class DepositForm(forms.Form):
-    deposit_amount = forms.IntegerField(label='kr', min_value=0, max_value=2000, widget=forms.NumberInput(attrs={'class': 'form-control',
-                                                                                                 'placeholder' : 'kr.'
-                                                                                                 }))
+    deposit_amount = forms.IntegerField(label='kr', min_value=0, max_value=2000,
+                                        widget=forms.NumberInput(attrs={'class': 'form-control',
+                                                                        'placeholder': 'kr.'}))
+
 
 class AddNameForm(forms.Form):
-    add_person = forms.CharField(label='', max_length=20, widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                              'placeholder' :'Navn'
-                                                                              }))
+    add_person = forms.CharField(label='', max_length=20,
+                                 widget=forms.TextInput(attrs={'class': 'form-control',
+                                                               'placeholder': 'Navn'}))
--- brus/liste/urls.py
+++ brus/liste/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls import url
 
 from . import views
@@ -7,8 +6,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<name_id>[0-9]+)/$', views.detail, name='navn'),
     url(r'^(?P<name_id>[0-9]+)/penger/$', views.penger, name='penger'),
-    url(r'^(?P<name_id>[0-9]+)/betale/$', views.pay, name='betale' ),
-    url(r'^(?P<name_id>[0-9]+)/deposit/$', views.deposit, name='deposit' ),
-    url(r'^addPerson/$',views.addPerson, name='addPerson'),
+    url(r'^(?P<name_id>[0-9]+)/betale/$', views.pay, name='betale'),
+    url(r'^(?P<name_id>[0-9]+)/deposit/$', views.deposit, name='deposit'),
+    url(r'^addPerson/$', views.addPerson, name='addPerson'),
     url(r'^(?P<name_id>[0-9]+)/delete/$', views.deletePerson, name='deletePerson')
 ]
