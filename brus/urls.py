03fb610c744f5f21fc2901370a5cca2ecee2adfe
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2016-05-01 01:28:07 +0200

    Adds redirect in urls and removes redirect in html

--- brus/urls.py
+++ brus/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('brus.liste.urls')),
     url(r'^accounts/login', 'django.contrib.auth.views.login', name='login'),
-    url(r'^accounts/logout', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^accounts/logout', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 ]
